b739910f14999ed6753e777ae3239d220a946e53
cre-yamashiro <yamashiro@hokkaido-ima.co.jp>
2019-12-15 13:01:26 +0900

    2019/12/15時点コミット

--- ccbc-comcomcoin/view/chat_coin.js
+++ ccbc-comcomcoin/view/chat_coin.js
@@ -1,261 +1,452 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import {
   StyleSheet,
   View,
   Image,
-  AsyncStorage,
   Text,
   ScrollView,
-  TextInput
-} from 'react-native'
-import {
-  Header,
-  Button,
-  Icon,
-  Avatar,
-  Card,
-  ListItem,
-  FormInput,
-  Divider
-} from 'react-native-elements'
-import { Table } from 'react-native-table-component'
-import InputNumberStyles from 'rmc-input-number/lib/styles'
-import InputNumber from 'rmc-input-number'
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableHighlight
+} from "react-native";
+import { Avatar, Card } from "react-native-elements";
+import RNPickerSelect from "react-native-picker-select";
+import BaseComponent from "./components/BaseComponent";
+import ConfirmDialog from "./components/ConfirmDialog";
+import AlertDialog from "./components/AlertDialog";
+import InAppHeader from "./components/InAppHeader";
 
-export default class ChatMsgForm extends Component {
-  state = {
-    open: false,
-    open2: false,
-    anchor: 'left',
-    activeStep1: {},
-    activeStep2: {},
-    activeStep3: {},
-    activeStep4: {},
-    activeStep5: {},
-    completed: {},
-    comment: '',
-    coin: 0,
-    tohyoCoin: 0,
-    headList: [],
-    resultList: [],
-    userid: null,
-    password: null,
-    tShainPk: 0,
-    imageFileName: null,
-    shimei: null,
-    kengenCd: null,
-    configCoin: 0,
-    text: ''
-  }
+const restdomain = require("./common/constans.js").restdomain;
+
+export default class ChatCoinForm extends BaseComponent {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.inputRefs = {};
+    this.state = {
+      comment: "",
+      resultList: [],
+      userid: null,
+      password: null,
+      loginShainPk: 0,
+      imageFileName: null,
+      shimei: null,
+      kengenCd: null,
+      fromShainPk: 0,
+      coinList: [],
+      target_manager: "",
+      bccoin: 0,
+      from_bcaccount: null,
+      sofuCoin: 0,
+      confirmDialogVisible: false,
+      confirmDialogMessage: "",
+      alertDialogVisible: false,
+      alertDialogMessage: "",
+      height: 0
+    };
   }
   /** コンポーネントのマウント時処理 */
-  async componentWillMount() {}
+  componentWillMount = async () => {
+    // ログイン情報の取得（BaseComponent）
+    await this.getLoginInfo();
 
-  onPressLogoutButton = () => {
-    this.props.navigation.navigate('Login')
-  }
-  onPressMenuButton = () => {
-    this.props.navigation.navigate('Menu')
-  }
-  onPressLoginGroupButton = () => {
-    this.props.navigation.navigate('LoginGroup')
-  }
-  onPressChatButton = () => {
-    this.props.navigation.navigate('Chat')
-  }
-  onPressChatSelectButton = () => {
-    this.props.navigation.navigate('ChatSelect')
-  }
-  onPressHomeButton = () => {
-    this.props.navigation.navigate('Home')
-  }
-  render() {
-    const { name, email } = this.state
-    const footerStyle = {
-      ...StyleSheet.flatten(styles.footerOverlay),
-      width: this.state.footerWidth
+    /** テスト用 */
+    /**　＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝*/
+    // this.setState({ userid: '1001' })
+    // this.setState({ password: '5555' })
+    // this.setState({ loginShainPk: 1 })
+    // this.state.userid = '1001'
+    // this.state.loginShainPk = 2
+    /**　＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝*/
+
+    // 前画面情報取得
+    // チャット相手
+    this.state.fromShainPk = this.props.navigation.getParam("fromShainPk");
+    // チャット相手氏名
+    this.state.shimei = this.props.navigation.getParam("shimei");
+    // チャット相手イメージファイル
+    this.state.imageFileName = this.props.navigation.getParam("image_file_nm");
+    // 送付コインリスト
+    this.setState({
+      coinList: [
+        {
+          label: "10コイン（ありがとう!）",
+          value: 10
+        },
+        {
+          label: "20コイン（うれしい!!）",
+          value: 20
+        },
+        {
+          label: "30コイン（感激!!!）",
+          value: 30
+        }
+      ]
+    });
+
+    // 初期表示情報取得
+    this.findChatCoin();
+  };
+
+  //画面初期表示情報取得
+  findChatCoin = async () => {
+    await fetch(restdomain + "/chat_coin/find", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: new Headers({ "Content-type": "application/json" })
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(
+        function(json) {
+          // 結果が取得できない場合は終了
+          if (typeof json.data === "undefined") {
+            return;
+          }
+          // 検索結果の取得
+          var dataList = json.data;
+          var resbccoin = json.bccoin;
+          var resfrom_bcaccount = json.from_bcaccount;
+
+          this.setState({ resultList: dataList });
+          this.setState({ bccoin: resbccoin });
+          this.setState({ from_bcaccount: resfrom_bcaccount });
+        }.bind(this)
+      )
+      .catch(error => console.error(error));
+  };
+
+  /** 送付ボタン押下 */
+  onClickSend = async () => {
+    // 入力チェック
+    var alertMessage = "";
+
+    // 送付コイン未入力チェック
+    if (
+      typeof this.state.target_manager === "undefined" ||
+      this.state.target_manager === null ||
+      (this.state.target_manager != null &&
+        this.state.target_manager.length === 0)
+    ) {
+      alertMessage += "送付するコイン数を選択してください\n\n";
     }
+
+    // コメント未入力チェック
+    if (
+      typeof this.state.comment === "undefined" ||
+      (this.state.comment != null && this.state.comment.length === 0)
+    ) {
+      alertMessage += "コメントを入力してください\n\n";
+    }
+
+    // コイン数チェック
+    if (this.state.target_manager > this.state.bccoin) {
+      alertMessage += "コインが不足しています";
+    }
+
+    // エラーメッセージを設定
+    if (alertMessage !== "") {
+      this.setState({
+        alertDialogVisible: true,
+        alertDialogMessage: alertMessage
+      });
+      return;
+    }
+    // 確認ダイアログを表示（YESの場合、send()を実行）
+    this.setState({
+      confirmDialogVisible: true,
+      confirmDialogMessage: "コインを送付します。よろしいですか？"
+    });
+  };
+
+  /** 確認ダイアログでYES押下 */
+  send = async () => {
+    this.setState({ confirmDialogVisible: false });
+
+    var coinComment = this.state.target_manager;
+    this.state.sofuCoin = coinComment;
+    coinComment =
+      "【" + coinComment + "コイン送付しました】\n " + this.state.comment;
+    this.state.comment = coinComment;
+
+    await fetch(restdomain + "/chat_coin/create", {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify(this.state),
+      headers: new Headers({ "Content-type": "application/json" })
+    })
+      .then(
+        function(response) {
+          return response.json();
+        }.bind(this)
+      )
+      .then(
+        function(json) {
+          if (!json.status) {
+            // TODO：エラー処理
+            alert("APIエラー");
+          } else {
+            this.props.navigation.navigate("ChatMsg", {
+              t_shain_Pk: this.state.fromShainPk,
+              shimei: this.state.shimei,
+              image_file_nm: this.state.imageFileName
+            });
+          }
+        }.bind(this)
+      )
+      .catch(error => console.error(error));
+  };
+
+  render() {
     return (
       <View style={styles.container}>
-        <Header
-          leftComponent={
-            <Icon
-              name={'chevron-left'}
-              type={'font-awesome'}
-              color="#fff"
-              onPress={this.onPressMenuButton}
-            />
-          }
-          centerComponent={
+        {/* -- 共有ヘッダ -- */}
+        <InAppHeader navigate={this.props.navigation.navigate} />
+        {/* -- 画面タイトル -- */}
+        <View style={[styles.screenTitleView, { flexDirection: "row" }]}>
+          {/* チャット相手 */}
+          <View style={{ alignItems: "center" }}>
+            <Text style={styles.screenTitleText}>{this.state.shimei}</Text>
+          </View>
+        </View>
+        <KeyboardAvoidingView behavior="padding">
+          <ScrollView>
             <View
               style={{
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center'
+                flexDirection: "row",
+                flexWrap: "nowrap"
               }}
             >
-              <View>
+              <View
+                style={{
+                  flexDirection: "column",
+                  flexWrap: "nowrap"
+                }}
+              >
                 <Text
                   style={{
-                    fontSize: 24,
-                    color: '#FFFFFF',
-                    textAlign: 'center'
+                    fontSize: 16,
+                    marginLeft: 16,
+                    marginTop: 20,
+                    color: "gray"
                   }}
                 >
-                  コイン送付
+                  所持コイン
                 </Text>
+                <Text
+                  style={{
+                    fontSize: 22,
+                    marginLeft: 16,
+                    textAlign: "center",
+                    color: "#FF683A",
+                    fontWeight: "bold"
+                  }}
+                >
+                  {this.state.bccoin}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    marginLeft: 16,
+                    marginTop: 20,
+                    color: "gray"
+                  }}
+                >
+                  送付相手
+                </Text>
+                <View style={{ flexDirection: "row", marginLeft: 16 }}>
+                  <Avatar
+                    rounded
+                    medium
+                    source={{
+                      uri: restdomain + `/uploads/${this.state.imageFileName}`
+                    }}
+                  />
+                  <Text
+                    style={{
+                      fontSize: 22,
+                      marginLeft: 16,
+                      marginTop: 20
+                    }}
+                  >
+                    {this.state.shimei}
+                  </Text>
+                </View>
               </View>
-            </View>
-          }
-          backgroundColor="#ff5622"
-        />
-        <ScrollView>
-          <View
-            style={{
-              flexDirection: 'row',
-              flexWrap: 'nowrap'
-            }}
-          >
-            <View
-              style={{
-                flexDirection: 'column',
-                flexWrap: 'nowrap'
-              }}
-            >
-              <Card>
-                <Avatar
-                  large
-                  rounded
-                  source={require('./../images/man6.jpg')}
-                  //activeOpacity={0.7}
-                  containerStyle={{ marginLeft: 10, marginRight: 10 }}
-                />
-                <Text style={{ fontSize: 20, textAlign: 'center' }}>
-                  瀬川　大和
-                </Text>
-                <Text style={{ fontSize: 16, textAlign: 'center' }}>
-                  11,200コイン
-                </Text>
-              </Card>
             </View>
             <Text
               style={{
-                fontSize: 32,
-                textAlign: 'center',
-                flex: 3,
-                marginTop: 120
+                fontSize: 16,
+                marginLeft: 16,
+                marginTop: 20,
+                color: "gray"
               }}
             >
-              >>
+              コイン
             </Text>
-            <Card>
-              <Avatar
-                large
-                rounded
-                source={require('./../images/woman4.jpg')}
-                //activeOpacity={0.7}
-                containerStyle={{ marginLeft: 10, marginRight: 10 }}
-              />
-              <Text style={{ fontSize: 20, textAlign: 'center' }}>
-                島崎　杏
-              </Text>
-            </Card>
-          </View>
-
-          <Text
-            style={{
-              fontSize: 16,
-              color: 'blue',
-              textAlign: 'left',
-              marginLeft: 54
-            }}
-          />
-          <Text style={{ fontSize: 20, marginLeft: 16, marginTop: 20 }}>
-            コイン
-          </Text>
-          <Card>
-            <InputNumber
-              min={0}
-              max={10000}
-              keyboardType="numeric"
-              value={300}
-              styles={InputNumberStyles}
+            <RNPickerSelect
+              placeholder={{
+                label: "送付するコイン数を選択してください",
+                value: ""
+              }}
+              items={this.state.coinList}
+              onValueChange={value => {
+                this.setState({
+                  target_manager: value
+                });
+              }}
+              style={{ ...pickerSelectStyles }}
+              value={this.state.target_manager}
+              ref={el => {
+                this.inputRefs.picker = el;
+              }}
             />
-          </Card>
+            <Text
+              style={{
+                fontSize: 16
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 16,
+                marginLeft: 16,
+                marginTop: 20,
+                color: "gray"
+              }}
+            >
+              コメント
+            </Text>
 
-          <Text
-            style={{
-              fontSize: 16,
-              color: 'blue',
-              textAlign: 'left',
-              marginLeft: 54
-            }}
-          />
-          <Text style={{ fontSize: 20, marginLeft: 16, marginTop: 20 }}>
-            コメント
-          </Text>
-          <Card>
+            {/* <Card> */}
             <TextInput
-              {...this.props}
+              // {...this.props}
               multiline={true}
+              numberOfLines={8}
+              // onContentSizeChange={event => {
+              //   this.setState({
+              //     height: event.nativeEvent.contentSize.height
+              //   })
+              // }}
+              // style={[
+              //   styles.default,
+              //   { height: Math.max(35, this.state.height), fontSize: 18 }
+              // ]}
+              style={[styles.inputText, { textAlignVertical: "top" }]}
+              value={this.state.comment}
+              // ref={component => (this._textinput = component)}
+              // underlineColorAndroid="transparent"
+              // returnKeyType="default"
+              // returnKeyType="done"
               onChangeText={text => {
                 this.setState({
                   comment: text
-                })
+                });
               }}
-              style={[
-                styles.default,
-                { height: Math.max(35, this.state.height), fontSize: 20 }
-              ]}
-              value={
-                'この前は仕事の相談に乗っていただきありがとうございました。\n先輩と会話したことによって、ずっと頭の中にいたモヤモヤが解消されました！！\n感謝の気持ちとしてコインを送らせていただきます。'
-              }
-              ref={component => (this._textinput = component)}
-              underlineColorAndroid="transparent"
             />
-          </Card>
-          <Text
-            style={{
-              fontSize: 16,
-              color: 'blue',
-              textAlign: 'left',
-              marginLeft: 54
-            }}
-          />
-          <Button
-            title="save"
-            icon={{ name: 'sign-in', type: 'font-awesome' }}
-          />
-        </ScrollView>
+            {/* </Card> */}
+
+            <Text
+              style={{
+                fontSize: 16
+              }}
+            />
+            <View style={{ flexDirection: "row" }}>
+              <View style={{ flex: 1 }}>
+                <TouchableHighlight onPress={() => this.onClickSend()}>
+                  <View style={styles.saveButton}>
+                    <Image
+                      source={require("./../images/coin_icon.png")}
+                      style={styles.menu_icon}
+                    />
+                    <View style={styles.articleTitleView}>
+                      <Text style={styles.articleTitleText}>送付する</Text>
+                    </View>
+                  </View>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+        {/* -- 確認ダイアログ -- */}
+        <ConfirmDialog
+          modalVisible={this.state.confirmDialogVisible}
+          message={this.state.confirmDialogMessage}
+          handleYes={this.send.bind(this)}
+          handleNo={() => {
+            this.setState({ confirmDialogVisible: false });
+          }}
+          handleClose={() => {
+            this.setState({ confirmDialogVisible: false });
+          }}
+        />
+        {/* -- メッセージダイアログ -- */}
+        <AlertDialog
+          modalVisible={this.state.alertDialogVisible}
+          message={this.state.alertDialogMessage}
+          handleClose={() => {
+            this.setState({ alertDialogVisible: false });
+          }}
+        />
       </View>
-    )
+    );
   }
 }
-
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingTop: 13,
+    paddingHorizontal: 10,
+    paddingBottom: 12,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 4,
+    backgroundColor: "white",
+    color: "black"
+  }
+});
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF'
-  },
-  header: {},
-  menu_item: {
-    flexDirection: 'row',
-    marginTop: 30,
-    marginLeft: 30,
-    marginRight: 30
+    backgroundColor: "#F5FCFF"
   },
   menu_icon: {
     width: 25,
     height: 25
   },
-  menu_button: {},
-  menu_icon_view: {},
-  menu_button_view: {
-    flex: 1,
-    flexDirection: 'column',
-    marginLeft: 10
+  saveButton: {
+    borderRadius: 20,
+    alignItems: "center",
+    marginTop: 30,
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor: "#ff9800",
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  articleTitleView: {
+    alignItems: "center"
+  },
+  articleTitleText: {
+    fontSize: 26,
+    color: "white",
+    padding: 10
+  },
+  screenTitleText: {
+    fontSize: 18,
+    color: "white",
+    padding: 10,
+    textAlign: "center",
+    alignSelf: "center"
+  },
+  screenTitleView: {
+    alignItems: "center",
+    backgroundColor: "#ff5622"
+  },
+  inputText: {
+    fontSize: 16,
+    color: "black",
+    padding: 5,
+    borderColor: "gray",
+    borderWidth: 1
   }
-})
+});
--- ccbc-comcomcoin/view/chat_select.js
+++ ccbc-comcomcoin/view/chat_select.js
@@ -1,202 +1,130 @@
-import React, { Component } from 'react'
-import {
-  StyleSheet,
-  View,
-  Image,
-  AsyncStorage,
-  Text,
-  ScrollView
-} from 'react-native'
-import {
-  Header,
-  Button,
-  Icon,
-  Avatar,
-  Card,
-  ListItem
-} from 'react-native-elements'
+import React from 'react'
+import { StyleSheet, View, ScrollView } from 'react-native'
+import { ListItem } from 'react-native-elements'
+import BaseComponent from './components/BaseComponent'
+import InAppHeader from './components/InAppHeader'
 
-const users = [
-  {
-    name: '安倍　大翔',
-    avatar: require('./../images/man1.jpg'),
-    cnt: 1
-  },
-  {
-    name: '伊藤　俊介',
-    avatar: require('./../images/man2.jpg'),
-    cnt: 9
-  },
-  {
-    name: '牛込　達也',
-    avatar: require('./../images/man3.jpg'),
-    cnt: 0
-  },
-  {
-    name: '江藤　蓮',
-    avatar: require('./../images/man4.jpg'),
-    cnt: 3
-  },
-  {
-    name: '織田　結月',
-    avatar: require('./../images/woman1.jpg'),
-    cnt: 2
-  },
-  {
-    name: '小川　結愛',
-    avatar: require('./../images/woman2.jpg'),
-    cnt: 2
-  },
-  {
-    name: '佐藤　結菜',
-    avatar: require('./../images/woman3.jpg'),
-    cnt: 6
-  },
-  {
-    name: '島崎　杏',
-    avatar: require('./../images/woman4.jpg'),
-    cnt: 2
-  },
-  {
-    name: '須藤　陽翔',
-    avatar: require('./../images/man5.jpg'),
-    cnt: 0
-  },
-  {
-    name: '瀬川　大和',
-    avatar: require('./../images/man6.jpg'),
-    cnt: 0
-  },
-  {
-    name: '曽我　湊',
-    avatar: require('./../images/man7.jpg'),
-    cnt: 1
-  },
-  {
-    name: '西村　新',
-    avatar: require('./../images/man8.jpg'),
-    cnt: 0
-  }
-]
+const restdomain = require('./common/constans.js').restdomain
 
-export default class ChatSelectForm extends Component {
-  state = {
-    open: false,
-    open2: false,
-    anchor: 'left',
-    activeStep1: {},
-    activeStep2: {},
-    activeStep3: {},
-    activeStep4: {},
-    activeStep5: {},
-    completed: {},
-    comment: {},
-    coin: 0,
-    tohyoCoin: 0,
-    headList: [],
-    resultList: [],
-    userid: null,
-    password: null,
-    tShainPk: 0,
-    imageFileName: null,
-    shimei: null,
-    kengenCd: null,
-    configCoin: 0
-  }
+export default class ChatSelectForm extends BaseComponent {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super()
+    this.state = {
+      resultList: [],
+      userid: null,
+      password: null,
+      loginShainPk: 0,
+      imageFileName: null,
+      shimei: null,
+      kengenCd: null
+    }
+    this.props = props
   }
-  /** コンポーネントのマウント時処理 */
-  async componentWillMount() {}
 
-  onPressLogoutButton = () => {
-    this.props.navigation.navigate('Login')
+  /** コンポーネントのマウント時処理 */
+  componentWillMount = async () => {
+    // ログイン情報の取得（BaseComponent）
+    await this.getLoginInfo()
+
+    /** テスト用 */
+    /**======================================== */
+    // this.setState({ userid: '1001' })
+    // this.setState({ password: '5555' })
+    // this.setState({ loginShainPk: 1 })
+    // this.state.loginShainPk = 2
+    /**======================================== */
+
+    // 初期表示情報取得
+    this.findChatUser()
   }
-  onPressMenuButton = () => {
-    this.props.navigation.navigate('Menu')
+
+  //画面初期表示情報取得
+  findChatUser = async () => {
+    await fetch(restdomain + '/chat_select/find', {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers: new Headers({ 'Content-type': 'application/json' })
+    })
+      .then(function(response) {
+        return response.json()
+      })
+      .then(
+        function(json) {
+          // 結果が取得できない場合は終了
+          if (typeof json.data === 'undefined') {
+            return
+          }
+          // 検索結果の取得
+          var dataList = json.data
+          this.setState({ resultList: dataList })
+        }.bind(this)
+      )
+      .catch(error => console.error(error))
   }
-  onPressLoginGroupButton = () => {
-    this.props.navigation.navigate('LoginGroup')
+
+  /** チャットユーザー選択 */
+  onPressChatMsgButton = (e, t_shain_Pk, shimei, image_file_nm) => {
+    // 画面遷移
+    this.props.navigation.navigate('ChatMsg', {
+      fromShainPk: t_shain_Pk,
+      shimei: shimei,
+      image_file_nm: image_file_nm
+    })
   }
-  onPressChatButton = () => {
-    this.props.navigation.navigate('Chat')
-  }
-  onPressChatSelectButton = () => {
-    this.props.navigation.navigate('ChatSelect')
-  }
-  onPressChatMsgButton = () => {
-    this.props.navigation.navigate('ChatMsg')
-  }
-  onPressHomeButton = () => {
-    this.props.navigation.navigate('Home')
-  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header
-          leftComponent={
-            <Icon
-              name={'chevron-left'}
-              type={'font-awesome'}
-              color="#fff"
-              onPress={this.onPressMenuButton}
-            />
-          }
-          centerComponent={
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}
-            >
-              <View>
-                <Text
-                  style={{
-                    fontSize: 24,
-                    color: '#FFFFFF',
-                    textAlign: 'center'
-                  }}
-                >
-                  チャット選択
-                </Text>
-              </View>
-            </View>
-          }
-          backgroundColor="#ff5622"
-          // style={styles.header}
-        />
+        {/* -- 共有ヘッダ -- */}
+        <InAppHeader navigate={this.props.navigation.navigate} />
+
         <ScrollView>
-          <Card containerStyle={{ padding: 0 }}>
-            {users.map((u, i) => {
-              if (u.cnt === 0) {
-                return (
-                  <ListItem
-                    key={i}
-                    roundAvatar
-                    title={u.name}
-                    titleStyle={{ fontSize: 20 }}
-                    avatar={u.avatar}
-                  />
-                )
-              } else {
-                return (
-                  <ListItem
-                    key={i}
-                    roundAvatar
-                    title={u.name}
-                    titleStyle={{ fontSize: 20 }}
-                    avatar={u.avatar}
-                    badge={{
-                      value: u.cnt,
-                      textStyle: { color: 'orange' }
-                    }}
-                  />
-                )
-              }
-            })}
-          </Card>
+          {/* -- リスト -- */}
+          {/* 未読が0件の場合はバッジを非表示にする */}
+          {this.state.resultList.map(n => {
+            if (n.new_info_cnt == 0) {
+              return (
+                <ListItem
+                  key={n.t_shain_pk}
+                  roundAvatar
+                  title={n.shimei}
+                  titleStyle={{ fontSize: 18 }}
+                  avatar={n.image_file_nm}
+                  onPress={e =>
+                    this.onPressChatMsgButton(
+                      e,
+                      `${n.t_shain_pk}`,
+                      `${n.shimei}`,
+                      `${n.image_file_nm}`
+                    )
+                  }
+                />
+              )
+            } else {
+              return (
+                <ListItem
+                  key={n.t_shain_pk}
+                  roundAvatar
+                  title={n.shimei}
+                  titleStyle={{ fontSize: 18 }}
+                  avatar={{ uri: restdomain + `/uploads/${n.image_file_nm}` }}
+                  badge={{
+                    value: n.new_info_cnt,
+                    textStyle: { color: '#FFFFFF' },
+                    containerStyle: { backgroundColor: '#ff5622' }
+                  }}
+                  onPress={e =>
+                    this.onPressChatMsgButton(
+                      e,
+                      `${n.t_shain_pk}`,
+                      `${n.shimei}`,
+                      `${n.image_file_nm}`
+                    )
+                  }
+                />
+              )
+            }
+          })}
         </ScrollView>
       </View>
     )
@@ -206,23 +134,5 @@ export default class ChatSelectForm extends Component {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F5FCFF'
-  },
-  header: {},
-  menu_item: {
-    flexDirection: 'row',
-    marginTop: 30,
-    marginLeft: 30,
-    marginRight: 30
-  },
-  menu_icon: {
-    width: 50,
-    height: 50
-  },
-  menu_button: {},
-  menu_icon_view: {},
-  menu_button_view: {
-    flex: 1,
-    flexDirection: 'column',
-    marginLeft: 10
   }
 })
