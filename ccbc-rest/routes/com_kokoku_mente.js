c9f496db4d4e5b08ccb395eb46e46451f3aa17e8
cre-inoue <inoue@hokkaido-ima.co.jp>
2020-08-27 19:50:14 +0900

    8/27 QA対応

--- ccbc-rest/routes/com_kokoku_mente.js
+++ ccbc-rest/routes/com_kokoku_mente.js
@@ -4,6 +4,17 @@ const async = require("async");
 var db = require("./common/sequelize_helper.js").sequelize;
 var db2 = require("./common/sequelize_helper.js");
 
+var multer = require("multer");
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "public/uploads/advertise");
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({ storage: storage });
+
 const query = (sql, params, res, req) => {
   if (req.body.db_name != null && req.body.db_name != "") {
     db = db2.sequelize3(req.body.db_name);
@@ -11,11 +22,13 @@ const query = (sql, params, res, req) => {
     db = require("./common/sequelize_helper.js").sequelize;
   }
 
-  db.query(sql, {
-    type: db.QueryTypes.RAW,
-  }).spread(async (datas, metadata) => {
-    res.json({ status: true, data: datas });
-  });
+  db
+    .query(sql, {
+      type: db.QueryTypes.RAW
+    })
+    .spread(async (datas, metadata) => {
+      res.json({ status: true, data: datas });
+    });
 };
 
 /**
@@ -39,7 +52,7 @@ router.post("/find", (req, res) => {
  * 新規登録
  *
  */
-router.post("/create", (req, res) => {
+router.post("/create", upload.fields([{ name: "image" }]), (req, res) => {
   console.log("◆create◆");
   if (req.body.db_name != null && req.body.db_name != "") {
     db = db2.sequelize3(req.body.db_name);
@@ -47,17 +60,18 @@ router.post("/create", (req, res) => {
     db = require("./common/sequelize_helper.js").sequelize;
   }
 
-  db.transaction(async function (tx) {
-    var resdatas = [];
-    console.log(req);
-    await tkokokuInsert(tx, resdatas, req);
-    res.json({ status: true, data: resdatas });
-  })
-    .then((result) => {
+  db
+    .transaction(async function(tx) {
+      var resdatas = [];
+      console.log(req);
+      await tkokokuInsert(tx, resdatas, req);
+      res.json({ status: true, data: resdatas });
+    })
+    .then(result => {
       // コミットしたらこっち
       console.log("正常");
     })
-    .catch((e) => {
+    .catch(e => {
       // ロールバックしたらこっち
       console.log("異常");
       console.log(e);
@@ -76,17 +90,18 @@ router.post("/edit", (req, res) => {
   } else {
     db = require("./common/sequelize_helper.js").sequelize;
   }
-  db.transaction(async function (tx) {
-    var resdatas = [];
-    console.log(req);
-    await tkokokuUpdate(tx, resdatas, req);
-    res.json({ status: true, data: resdatas });
-  })
-    .then((result) => {
+  db
+    .transaction(async function(tx) {
+      var resdatas = [];
+      console.log(req);
+      await tkokokuUpdate(tx, resdatas, req);
+      res.json({ status: true, data: resdatas });
+    })
+    .then(result => {
       // コミットしたらこっち
       console.log("正常");
     })
-    .catch((e) => {
+    .catch(e => {
       // ロールバックしたらこっち
       console.log("異常");
       console.log(e);
@@ -105,17 +120,18 @@ router.post("/delete", (req, res) => {
   } else {
     db = require("./common/sequelize_helper.js").sequelize;
   }
-  db.transaction(async function (tx) {
-    var resdatas = [];
-    console.log(req);
-    await tkokokuDelete(tx, resdatas, req);
-    res.json({ status: true, data: resdatas });
-  })
-    .then((result) => {
+  db
+    .transaction(async function(tx) {
+      var resdatas = [];
+      console.log(req);
+      await tkokokuDelete(tx, resdatas, req);
+      res.json({ status: true, data: resdatas });
+    })
+    .then(result => {
       // コミットしたらこっち
       console.log("正常");
     })
-    .catch((e) => {
+    .catch(e => {
       // ロールバックしたらこっち
       console.log("異常");
       console.log(e);
@@ -134,23 +150,25 @@ function tkokokuInsert(tx, resdatas, req) {
       "insert into t_kokoku (file_path, comment, delete_flg, insert_user_id, insert_tm, update_user_id, update_tm) " +
       "VALUES (?, ?, ?, ?, ?, ?, current_timestamp) ";
 
-    db.query(sql, {
-      transaction: tx,
-      replacements: [
-        req.body.title,
-        req.body.comment,
-        req.body.notice_dt,
-        "0",
-        req.body.userid,
-        null,
-        null,
-      ],
-    }).spread((datas, metadata) => {
-      console.log("◆◆◆");
-      console.log(datas);
-      resdatas.push(datas);
-      return resolve(datas);
-    });
+    db
+      .query(sql, {
+        transaction: tx,
+        replacements: [
+          req.body.title,
+          req.body.comment,
+          req.body.notice_dt,
+          "0",
+          req.body.userid,
+          null,
+          null
+        ]
+      })
+      .spread((datas, metadata) => {
+        console.log("◆◆◆");
+        console.log(datas);
+        resdatas.push(datas);
+        return resolve(datas);
+      });
   });
 }
 
@@ -166,21 +184,23 @@ function tkokokuUpdate(tx, resdatas, req) {
       "update t_kokoku set file_path = ?, comment = ?" +
       "update_user_id = ?, update_tm = current_timestamp WHERE renban = ?";
 
-    db.query(sql, {
-      transaction: tx,
-      replacements: [
-        req.body.title,
-        req.body.comment,
-        req.body.notice_dt,
-        req.body.userid,
-        req.body.renban,
-      ],
-    }).spread((datas, metadata) => {
-      console.log("◆◆◆◆");
-      console.log(datas);
-      resdatas.push(datas);
-      return resolve(datas);
-    });
+    db
+      .query(sql, {
+        transaction: tx,
+        replacements: [
+          req.body.title,
+          req.body.comment,
+          req.body.notice_dt,
+          req.body.userid,
+          req.body.renban
+        ]
+      })
+      .spread((datas, metadata) => {
+        console.log("◆◆◆◆");
+        console.log(datas);
+        resdatas.push(datas);
+        return resolve(datas);
+      });
   });
 }
 
@@ -194,15 +214,17 @@ function tkokokuDelete(tx, resdatas, req) {
   return new Promise((resolve, reject) => {
     var sql = "update t_kokoku set delete_flg = '1' WHERE renban = ?";
 
-    db.query(sql, {
-      transaction: tx,
-      replacements: [req.body.renban],
-    }).spread((datas, metadata) => {
-      console.log("◆◆◆◆◆");
-      console.log(datas);
-      resdatas.push(datas);
-      return resolve(datas);
-    });
+    db
+      .query(sql, {
+        transaction: tx,
+        replacements: [req.body.renban]
+      })
+      .spread((datas, metadata) => {
+        console.log("◆◆◆◆◆");
+        console.log(datas);
+        resdatas.push(datas);
+        return resolve(datas);
+      });
   });
 }
 
