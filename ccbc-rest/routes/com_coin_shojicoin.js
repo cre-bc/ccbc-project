99189efa3a6180a4cffd8e34850cc38a3610a46a
y.sasaki <sasaki@hokkaido-ima.co.jp>
2020-09-08 02:31:38 +0900

    9/8 不具合修正

--- ccbc-rest/routes/com_coin_shojicoin.js
+++ ccbc-rest/routes/com_coin_shojicoin.js
@@ -6,7 +6,6 @@ var db = require("./common/sequelize_helper.js").sequelize;
 var db2 = require("./common/sequelize_helper.js");
 const bcdomain = require("./common/constans.js").bcdomain;
 
-// ----------------------------------------------------------------------
 /**
  * API : findgraphcoin
  * 所持コイン一覧に表示する情報を取得
@@ -17,7 +16,6 @@ router.post("/findshojicoin", (req, res) => {
   console.log("API : findshojicoin - end");
 });
 
-// ----------------------------------------------------------------------
 /**
  * データ取得用関数（グラフ情報取得）
  * グラフの要素は、社員と所持コインのみ
@@ -26,49 +24,23 @@ router.post("/findshojicoin", (req, res) => {
  */
 async function findshojicoin(req, res) {
   var resdatas = [];
-  var resbccoin = [];
-  var sakicoin_sum = 0;
+  var bccoin = 0
 
   console.log("API : findshojicoin →中身");
 
-  //社員リストの情報取得
-  resdatas = await getshainList(db, req);
-  //贈与テーブルより、取引情報取得（所持コイン）
-  resbccoin = await getshojicoinList(db, req);
+  // BCアカウントを取得
+  resdatas = await bcAccountGet(db, req)
 
-  console.log(resdatas);
-  console.log(resbccoin);
-
-  var trans = []
-  var lengthData = []
   for (let i in resdatas) {
-    var cnt = 0
-    for (let n in resbccoin) {
-      if (resdatas[i].t_shain_pk === resbccoin[n].zoyo_saki_shain_pk) {
-        trans.push(resbccoin[n].transaction_id)
-        cnt++
-      }
+    var param = {
+      account: resdatas[i].bc_account,
+      bc_addr: req.body.bc_addr
     }
-    lengthData.push(cnt)
-  }
-  var param = {
-    transaction: trans
-  }
-  var sakicoin = await bctransactionsget(param)
-
-  var index = 0
-  for (var i in resdatas) {
-    var length = index + lengthData[i]
-    for (var j = index; j < length; j++) {
-      sakicoin_sum += sakicoin.body.trans[j].coin
-    }
-    // 社員に紐づく受領コインの合計をセット
-    resdatas[i].sakicoin = sakicoin_sum
-    sakicoin_sum = 0
-    index = length
+    // BCコイン数を取得
+    bccoin = await bccoinget(param)
+    resdatas[i].sakicoin = bccoin
   }
 
-  // この時点で、社員リストを基としたresdatasには、社員に紐づく所持コイン（sakicoin）が紐づいている
   console.log(resdatas);
   res.json({
     status: true,
@@ -76,24 +48,23 @@ async function findshojicoin(req, res) {
   });
 }
 
-// ----------------------------------------------------------------------
 /**
  * テーブルよりselect（DBアクセス）
  * グラフ表示用の情報取得（グラフの基本となる社員情報）
  * @param db SequelizeされたDBインスタンス
  * @param req リクエスト
  */
-function getshainList(db, req) {
+function bcAccountGet(db, req) {
   return new Promise((resolve, reject) => {
     // SQLとパラメータを指定
-    console.log("API : getshainList →中身");
-    console.log(req.body.sort_graph);
+    console.log("API : bcAccountGet →中身");
 
     var sql =
       "select " +
       "tsha.t_shain_pk" +
       ", tsha.shimei" +
       ", tsha.shimei_kana " +
+      ", tsha.bc_account as bc_account " +
       "from " +
       "t_shain tsha " +
       "where tsha.delete_flg = '0' " +
@@ -113,72 +84,27 @@ function getshainList(db, req) {
   });
 }
 
-// ----------------------------------------------------------------------
-// 所持コイン一覧は、検索条件なしで、社員とコイン情報のみ取得しグラフに表示
 /**
- * テーブルよりselect（DBアクセス）
- * @param db SequelizeされたDBインスタンス
- * @param req リクエスト
- */
-function getshojicoinList(db, req) {
-  return new Promise((resolve, reject) => {
-    // SQLとパラメータを指定
-    var sql =
-      "select " +
-      "tzoyo.zoyo_moto_shain_pk" +
-      ", tsha1.shimei AS shimei_saki" +
-      ", tsha1.shimei_kana AS shimei_kana_saki" +
-      ", tzoyo.zoyo_saki_shain_pk" +
-      ", tsha2.shimei AS shimei_moto" +
-      ", tsha2.shimei_kana AS shimei_kana_moto" +
-      ", tzoyo.zoyo_comment AS event" +
-      ", tzoyo.transaction_id" +
-      ", tsha1.t_shain_pk as t_shain_pk" +
-      ", tsha1.shimei as shimei" +
-      ", tsha1.shimei_kana as shimei_kana" +
-      ", tsha1.bc_account as bc_account" +
-      ", tsha1.kengen_cd as kengen_cd " +
-      "from t_zoyo tzoyo " +
-      "left join t_shain tsha1 " +
-      "on tsha1.t_shain_pk = tzoyo.zoyo_moto_shain_pk " +
-      "left join t_shain tsha2 " +
-      "on tsha2.t_shain_pk = tzoyo.zoyo_saki_shain_pk " +
-      "where tzoyo.delete_flg = '0';"
-    db
-      .query(sql, {
-        replacements: { sort_graph: req.body.sort_graph },
-        type: db.QueryTypes.RAW
-      })
-      .spread((datas, metadata) => {
-        console.log("DBAccess : getshojicoinList result...");
-        console.log(datas);
-        return resolve(datas);
-      });
-  });
-}
-
-// ----------------------------------------------------------------------
-/**
- * 取引情報取得用関数
+ * BCコイン取得用関数
  * @param {*} param
- * 受領コインと使用コインをグラフに出力用
  */
-
-function bctransactionsget(param) {
+function bccoinget(param) {
   return new Promise((resolve, reject) => {
+    console.log('★start bccoinget★')
     request
-      .post(bcdomain + "/bc-api/get_transactions")
+      .post(bcdomain + '/bc-api/get_coin')
       .send(param)
       .end((err, res) => {
-        console.log("★★★");
+        console.log('★★★')
         if (err) {
-          console.log("★" + err);
-          return;
+          console.log('★' + err)
+          return
         }
-        console.log("★★★" + res.body.trans);
-        return resolve(res);
-      });
-  });
+        console.log('★★★' + res.body.coin)
+        return resolve(res.body.coin)
+      })
+    console.log('★end bccoinget★')
+  })
 }
 
 module.exports = router;
